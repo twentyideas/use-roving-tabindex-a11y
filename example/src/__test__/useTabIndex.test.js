f404a4896d83090cbed0c5f02b30d151f233368a
Cesar Garcia <cesargarcia@Cesars-MacBook-Pro.local>
2022-01-11 13:30:43 -0800

    added arrow press tests

--- example/src/__test__/useTabIndex.test.js
+++ example/src/__test__/useTabIndex.test.js
@@ -1,44 +1,20 @@
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, getByRole, getByTestId, getByText, render, screen } from '@testing-library/react'
+import {
+  fireEvent,
+  getByRole,
+  getByTestId,
+  getByText,
+  render,
+  screen,
+} from '@testing-library/react'
 import { renderHook, act } from '@testing-library/react-hooks'
 import TestSimpleList from '../TestSimpleList'
 import List from '../List'
 import { useRovingTabIndex } from '../../../dist'
 import useCounter from '../useCounter'
 import { SimpleList } from '../SimpleList'
+import { ComplexList } from '../ComplexList'
 
-// test or it to make a test block
-// test component
-// find element, using screen to get element
-// interact with element
-// assert results as 
-
-// const listRef = useRovingTabIndex();
-
-
-// const mockListWithRef = () => {
-//     return (
-//         <div>
-//           <h3 data-testid={'data'} >Simple List</h3>
-//           <ul ref={listRef}>
-//             <li tabIndex={0}>1</li>
-//           </ul>
-//         </div>
-//       )
-// }
-  
-
-it('should render header', () => {
-  render(<TestSimpleList />)
-  const HeadingElement = screen.getByTestId(/simplist/i)
-  expect(HeadingElement).toBeInTheDocument()
-})
-
-it('should render the list item with the value of 1', async () => {
-  render(<List />)
-  const listElement = screen.getByText(/1/i)
-  expect(listElement).toBeInTheDocument()
-})
 // passes test
 // it('should render the list in focus', async () => {
 //   render(<List/>)
@@ -50,9 +26,9 @@ it('should render the list item with the value of 1', async () => {
 //   expect(refElement).toBeInTheDocument()
 // })
 
-// testing just the hook 
-// result returns an object cant invoke the hook without a list 
-// 
+// testing just the hook
+// result returns an object cant invoke the hook without a list
+//
 // it('focusses on list item with index of 0', () => {
 //   const {result} = renderHook(
 //     () => useRovingTabIndex()
@@ -77,49 +53,58 @@ it('should render the list item with the value of 1', async () => {
 
 //   //   }
 //   // })
-//   // const buttonElement = getByTestId('btn') 
+//   // const buttonElement = getByTestId('btn')
 //   const list = simpleRef.ref;
 
 //   expect(simpleRef).toBeInTheDocument();
 //   expect(list)
 // })
 
-
-// finally able to locate the list items! woop woop. 
+// finally able to locate the list items! woop woop.
 // now to test if theyre in focus
-it('should render the simplelist component', async ()=> {
-    render(<TestSimpleList />);
-    // const listElement = screen.getByText(/All the things you could ever want to know/i)
-    // expect(listElement).toBeInTheDocument();
-    const cmpnt =  screen.getAllByText('All the things you could ever want to know')
-    expect(cmpnt[0]).toBeInTheDocument()
+it('should render the simplelist component', async () => {
+  render(<TestSimpleList />)
+  // const listElement = screen.getByText(/All the things you could ever want to know/i)
+  // expect(listElement).toBeInTheDocument();
+  const cmpnt = screen.getAllByText(
+    'All the things you could ever want to know'
+  )
+  expect(cmpnt[0]).toBeInTheDocument()
 })
 
 // look for anchor tag, not the componenet
-// <a href="#" tabindex="0"><h4>Item 2</h4></a> 
-it('should render the simplelist component', async ()=> {
-  render(<SimpleList />);
+// <a href="#" tabindex="0"><h4>Item 2</h4></a>
+it('should render the simplelist component', async () => {
+  render(<SimpleList />)
   // const listElement = screen.getByText(/All the things you could ever want to know/i)
   // expect(listElement).toBeInTheDocument();
   // const listElement = getByRole()
-  const cmpnt =  screen.getAllByText('All the things you could ever want to know')
-  const listItem1 = cmpnt[0];
-  
+  const cmpnt = screen.getAllByText(
+    'All the things you could ever want to know'
+  )
+  const listItem1 = cmpnt[0]
+
   // fireEvent.keyDown(btnCmpt, {keyDown: "Tab",code:"Tab", charCode: 9 })
-  fireEvent.keyDown(listItem1, {key: 'ArrowDown', code: 'ArrowDown', charCode: 40})
+  fireEvent.keyDown(listItem1, {
+    key: 'ArrowDown',
+    code: 'ArrowDown',
+    charCode: 40,
+  })
 
-    // expect(listItem1).toHaveFocus();
-
-
+  // expect(listItem1).toHaveFocus();
 })
 
-
-// locate anchor tags, not headers 
+// locate anchor tags, not headers
 // searching for text item finds headers
+// find the second tag after hitting arrowdown!
 it('should find the anchor tags rendered on the screen', async () => {
   render(<SimpleList />)
   const anchorElements = screen.getAllByRole('link')
-  fireEvent.keyDown(anchorElements[0], {key: 'ArrowDown', code: 'ArrowDown', charCode: 40})
+  fireEvent.keyDown(anchorElements[0], {
+    key: 'ArrowDown',
+    code: 'ArrowDown',
+    charCode: 40,
+  })
 
   expect(anchorElements[1]).toHaveFocus()
 })
@@ -134,13 +119,41 @@ test('should increment counter', () => {
   expect(result.current.count).toBe(1)
 })
 
+// testing for arrowing down to focus the second item on the list
+it('should render the complex list and find the second item on the list', () => {
+  render(<ComplexList />)
+  const anchorElements = screen.getAllByRole('link')
+  fireEvent.keyDown(anchorElements[0], {
+    key: 'ArrowDown',
+    code: 'ArrowDown',
+    charCode: 40,
+  })
+  expect(anchorElements[1]).toHaveFocus()
+
+})
+
+// testing for arrow right to focus on the subscribe button
+
+it('should render the complex list and find the second item on the list', () => {
+  render(<ComplexList />)
+  const anchorElements = screen.getAllByRole('link');
+  const headerElement = screen.getAllByRole('heading');
+  fireEvent.keyDown(anchorElements[0], {
+    key: 'ArrowDown',
+    code: 'ArrowDown',
+    charCode: 40,
+  })
+
+  fireEvent.keyDown(anchorElements[0], {
+    key: 'ArrowRight',
+    code: 'ArrowRight',
+    charCode: 39,
+  })
 
 
+  const btnElement = screen.getAllByRole('button');
 
-// it('hopefully works', async () => {
-//     const { listRef } = renderHook(()=> useRovingTabIndex())
-//     act(()=>{
-//         mockListWithRef.useFocusOnMount()
-//     })
-//     expect(headerElement).toBeInTheDocument();
-// })
+  // expect(anchorElements[1]).toHaveFocus()
+  expect(btnElement[1]).toHaveFocus()
+
+})
